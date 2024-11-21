f8d7e8585dd9339ffa7f3b2217090a33be755702
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-11-21 12:41:08 +0700

    Update main.go Add String iteration: runes or bytes

--- main.go
+++ main.go
@@ -8,4 +8,14 @@ func main() {
     for i, s := range a {
         fmt.Println(i, s)
     }
+
+    // String iteration: runes or bytes
+    for i, ch := range "日本語" {
+        fmt.Printf("%#U starts at byte position %d\n", ch, i)
+    }
+
+    const s = "日本語"
+    for i := 0; i < len(s); i ++ {
+        fmt.Printf("%x ", s[i])
+    }
 }
